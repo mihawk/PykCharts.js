806ef7d0d04cc502fd645245fe33ff99b8b9be89
nehalgala <nehal.gala@pykih.com>
2014-10-31 12:09:52 +0530

    #105 adding error handling for pictograph

--- src/js/other/other.js
+++ src/js/other/other.js
@@ -39,8 +39,6 @@ PykCharts.other.processInputs = function (chartObject, options) {
         , functionality = theme.functionality
         , otherCharts = theme.otherCharts;
 
-    chartObject.assets_location = options.pykih_charts_assets_location ? options.pykih_charts_assets_location : stylesheet.pykih_charts_assets_location;
-
     chartObject.selector = options.selector ? options.selector : stylesheet.selector;
     if(options.chart_width) {
         var targetWidth = $(options.selector).width();
@@ -90,16 +88,16 @@ PykCharts.other.processInputs = function (chartObject, options) {
     // chartObject.credit_my_site_url = options.credit_my_site_url ? options.credit_my_site_url : stylesheet.credit_my_site_url;
     chartObject.data_source_name = options.data_source_name ? options.data_source_name : "";
     chartObject.data_source_url = options.data_source_url ? options.data_source_url : "";
-
+    
     chartObject.transition_duration = options.transition_duration ? options.transition_duration : functionality.transition_duration;
-
+    
     chartObject.background_color = options.background_color ? options.background_color.toLowerCase() : stylesheet.background_color;
-
+    
     chartObject.fullscreen_enable = options.fullscreen_enable ? options.fullscreen_enable : stylesheet.fullscreen_enable;
     chartObject.loading = options.loading_gif_url ? options.loading_gif_url: stylesheet.loading_gif_url;
-
+    
     chartObject.export_enable = options.export_enable ? options.export_enable.toLowerCase() : stylesheet.export_enable;
-    // chartObject.export_image_url = options.export_image_url ? options.export_image_url : stylesheet.export_image_url;
+    // chartObject.export_image_url = options.export_image_url ? options.export_image_url : stylesheet.export_image_url; 
     chartObject.k = new PykCharts.Configuration(chartObject);
 
     chartObject.k.validator().validatingSelector(chartObject.selector.substring(1,chartObject.selector.length))
@@ -109,7 +107,8 @@ PykCharts.other.processInputs = function (chartObject, options) {
                 .validatingDataType(chartObject.subtitle_size,"subtitle_size")
                 .validatingDataType(chartObject.transition_duration,"transition_duration")
                 .validatingFontWeight(chartObject.title_weight,"title_weight")
-                .validatingFontWeight(chartObject.subtitle_weight,"subtitle_weight");
+                .validatingFontWeight(chartObject.subtitle_weight,"subtitle_weight")
+                .validatingColor(chartObject.background_color,"background_color")                                                              
 
     return chartObject;
 };
