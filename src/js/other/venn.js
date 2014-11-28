1c9723f20bdea0029bb7e575df33bc1fd8884c4d
Devanshi Sanghani (Pykih Staff) <devanshi.sanghani@pykih.com>
2014-11-28 15:29:36 +0530

    pulling code for sync

---
+++ src/js/other/venn.js
@@ -0,0 +1,89 @@
+PykCharts.other.venn = function (options) {
+    var that = this;
+
+    this.execute = function () {
+      d3.json(options.data[0], function (data) {
+          that.sets = data;
+          d3.json(options.data[1], function (data) {
+              that.overlaps = data;
+              that.render();
+          })
+      })
+    }
+    this.render = function () {
+        // get positions for each set
+        sets = venn.venn(that.sets, that.overlaps);
+
+        // draw the diagram in the 'venn' div
+        var diagram = venn.drawD3Diagram(d3.select(options.selector), sets, 500, 500);
+
+        // add a tooltip showing the size of each set/intersection
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "venntooltip");
+
+        d3.selection.prototype.moveParentToFront = function() {
+          return this.each(function(){
+            this.parentNode.parentNode.appendChild(this.parentNode);
+          });
+        };
+
+        // hover on all the circles
+        diagram.circles
+            .style("stroke-opacity", 0)
+            .style("stroke", "white")
+            .style("stroke-width", "2");
+
+        diagram.nodes
+            .on("mousemove", function() {
+                tooltip.style("left", (d3.event.pageX) + "px")
+                       .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseover", function(d, i) {
+                var selection = d3.select(this).select("circle");
+                selection.moveParentToFront()
+                    .transition()
+                    .style("fill-opacity", .5)
+                    .style("stroke-opacity", 1);
+
+                tooltip.transition().style("opacity", .9);
+                tooltip.text(d.weight + " users");
+            })
+            .on("mouseout", function(d, i) {
+                d3.select(this).select("circle").transition()
+                    .style("fill-opacity", .3)
+                    .style("stroke-opacity", 0);
+                tooltip.transition().style("opacity", 0);
+            });
+
+        // draw a path around each intersection area, add hover there as well
+        diagram.svg.selectAll("path")
+            .data(overlaps)
+            .enter()
+            .append("path")
+            .attr("d", function(d) {
+                return venn.intersectionAreaPath(d.sets.map(function(j) { return sets[j]; }));
+            })
+            .style("fill-opacity","0")
+            .style("fill", "black")
+            .style("stroke-opacity", 0)
+            .style("stroke", "white")
+            .style("stroke-width", "2")
+            .on("mouseover", function(d, i) {
+                d3.select(this).transition()
+                    .style("fill-opacity", .1)
+                    .style("stroke-opacity", 1);
+                tooltip.transition().style("opacity", .9);
+                tooltip.text(d.weight + " users");
+            })
+            .on("mouseout", function(d, i) {
+                d3.select(this).transition()
+                    .style("fill-opacity", 0)
+                    .style("stroke-opacity", 0);
+                tooltip.transition().style("opacity", 0);
+            })
+            .on("mousemove", function() {
+                tooltip.style("left", (d3.event.pageX) + "px")
+                       .style("top", (d3.event.pageY - 28) + "px");
+            })
+    }
+}
