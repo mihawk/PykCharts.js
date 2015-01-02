4be459d32493cf06191b1253926acf5f39ab5deb
nehalgala <nehal.gala@pykih.com>
2015-01-02 12:15:08 +0530

    resovling theissue Election Pie and donut- Labels are overlapping with visual elements

--- dist/pykcharts.1.0.0.js
+++ dist/pykcharts.1.0.0.js
@@ -5302,9 +5302,11 @@ PykCharts.oneD.pieFunctions = function (options,chartObject,type) {
                                     return "end";
                                 } else if (rads>=6) {
                                     return "middle";
+                                } else if(type ==="election pie" || type === "election donut" && rads < -1) {
+                                    return "end";
                                 } else if(rads<0) {
                                     return "middle";
-                                }
+                                } 
                             })
                             .attr("dy",5)
                             .attr("pointer-events","none")
@@ -5315,6 +5317,7 @@ PykCharts.oneD.pieFunctions = function (options,chartObject,type) {
 
                         tick_label.exit().remove();
                     },that.transitions.duration());
+
 
                     var tick_line = that.group.selectAll("line")
                         .data(that.pie(that.new_data));
