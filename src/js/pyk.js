133d44f2b647ef7439b623d32e7dd8fe86b51a1f
ronakrrb <rb@pykih.com>
2015-03-02 21:14:48 +0530

    Updated licence

--- src/js/pyk.js
+++ src/js/pyk.js
@@ -1,22 +1,17 @@
-/* ====================================================
-* This file is part of PykCharts v1.0.0
-* Copyright 2014 Pykih Software LLP
-* Contact: charts@pykih.com
-* ======================================================
-*
-* Licensees holding valid commercial licenses may use
-* this file in accordance with the Commercial Software
-* License Agreement provided with the Software or,
-* alternatively, in accordance with the terms contained
-* in a written agreement between you and Pykih.
-*
-* https://chartstore.io/license
-*
-* If you are unsure which license is appropriate for
-* your use, please contact the sales department at
-* charts@pykih.com
-*
-* ====================================================== */
+/* ======================================================
+Copyright (c) 2015, Pykih Software LLP
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+====================================================== */
 
 var PykCharts = {};
 PykCharts.assets = "../pykih-charts/assets/";
@@ -443,7 +438,7 @@ PykCharts.Configuration = function (options){
                 for (; i<condition_checking ; i+=increment_counter) {
                     element_node = xml.firstChild.childNodes[i];
                     element_object = {};
-                    
+
                     for(j=0, element_len=element_node.childElementCount ; j<element_len ; j++) {
                         data_point = element_node.children.item(j);
                         key_of_data_point = data_point.nodeName;
@@ -511,7 +506,7 @@ PykCharts.Configuration = function (options){
                     value = element.style[styleAttribute];
                     if(!value) {
                         value = d3.select(element).style(styleAttribute);
-                    }                    
+                    }
                     if(value) {
                         return;
                     } else {
@@ -988,7 +983,7 @@ PykCharts.Configuration = function (options){
                     if (data.indexOf("<root>") != -1) {
                         var xml_data_converted_from_string,
                             json_data_converted_from_xml;
-                        
+
                         if (window.DOMParser) {
                             parser = new DOMParser();
                             xml_data_converted_from_string = parser.parseFromString(data,"text/xml");
